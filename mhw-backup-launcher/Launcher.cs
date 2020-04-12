3f8203558e61e828e54facfe58c943e91b882ce1
Isaac Ouellet Therrien <isaac.computing@gmail.com>
2020-04-12 11:40:59 -0400

    Basically wrote everything

--- mhw-backup-launcher/Launcher.cs
+++ mhw-backup-launcher/Launcher.cs
@@ -1,26 +1,64 @@
 using System;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Security;
-using System.Security.AccessControl;
-using System.Security.Principal;
-using System.Threading;
-using System.Windows.Forms;
 
 namespace mhw_backup_launcher {
   internal static class Launcher {
-    private const int MHW_STEAM_APP_ID = 582010;
+    private const string MHW_STEAM_APP_ID = "582010";
+
+    // TODO: Args
+
     private static void Main(string[] args) {
-      //Starts THUD
-      StartMhw(MHW_STEAM_APP_ID);
+      try {
+        BackupMhwSaveFiles(STEAM_INSTALL_PATH, STEAM_USER_ID, MHW_STEAM_APP_ID, STEAM_USERNAME, @"G:\Backup");
+      } catch (Exception err) {
+        Console.Write(err);
+        Console.Write("\nPress any key to continue...");
+        Console.ReadLine();
+      }
+
+      //StartMhw(MHW_STEAM_APP_ID);
+    }
+
+    private static void BackupMhwSaveFiles(string steamInstallPath, string steamUserId, string mhwSteamAppId, string steamUsername, string backupFolderPath) {
+      string mhwSaveFolder = Path.Combine(
+        steamInstallPath,
+        "userdata",
+        steamUserId,
+        mhwSteamAppId,
+        "remote"
+      );
+
+      string backupFolderNowPath = Path.Combine(
+        backupFolderPath,
+        steamUsername,
+        "Monster Hunter World",
+        DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss")
+      );
+
+      DirectoryCopy(mhwSaveFolder, backupFolderNowPath);
+    }
+
+    private static void DirectoryCopy (string sourceDirName, string destDirName) {
+      DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+      if (!dir.Exists) {
+        throw new DirectoryNotFoundException(
+          $"Source directory does not exist or could not be found: {sourceDirName}"
+        );
+      }
+
+      Directory.CreateDirectory(destDirName);
+
+      FileInfo[] files = dir.GetFiles();
+      foreach (FileInfo file in files) {
+        file.CopyTo(Path.Combine(destDirName, file.Name), false);
+      }
     }
 
     private static void StartMhw(int mhwSteamAppId) {
-      var mhwStartInfo = new ProcessStartInfo
-      {
-          FileName = $"steam://rungameid/{mhwSteamAppId}",
+      var mhwStartInfo = new ProcessStartInfo {
+        FileName = $"steam://rungameid/{mhwSteamAppId}",
       };
       Process.Start(mhwStartInfo);
     }
